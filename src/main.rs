64707acc998797def01f8f72d905f347d9c457e9
John FitzGerald <jfitzgerald@zoll.com>
2022-07-05 13:46:39 -0700

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 // python -m serial.tools.list_ports -v
 
 // Common Baud Rates:
-// 110,& 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000 and 256000
+// 110, 300, 600, 1200, 2400, 4800, 9600, 14400,
+// 19200, 38400, 57600, 115200, 128000 and 256000 (we use 3000000)
 
 use jfconsole::ProcessorConfig;
 
 pub fn main() {
-    ProcessorConfig::user_select()
-        .unwrap()
-        .main_task()
-        .unwrap();
+    let op = |cfg: ProcessorConfig| cfg.main_task();
+    match ProcessorConfig::user_select().and_then(op) {
+        Ok(_) => println!("> [main] end"),
+        Err(e) => println!("> [main] error {:#?}", e),
+    }
 }
