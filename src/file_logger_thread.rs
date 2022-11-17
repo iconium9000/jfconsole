6746220565e4b0fc95f1cd5318569d2773f10d85
jfitzgerald <jfitzgerald@zoll.com>
2022-11-17 14:58:44 -0800

    changed line recv timeout

--- src/file_logger_thread.rs
+++ src/file_logger_thread.rs
@@ -70,7 +70,7 @@ fn file_logger_task(
 ) -> BoxResult<()> {
     let mut synced = true;
     Ok(while victim.is_alive() {
-        let duration = Duration::from_millis(200);
+        let duration = Duration::from_millis(100);
         if let Ok(mut line) = line_receiver.recv_timeout(duration) {
             line.push('\n');
             if let Err(e) = file.write_all(line.as_bytes()) {
